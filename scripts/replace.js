9604cd7da8791b230ad2e8acc0cc5e89ed2d8d0a
FoxerLee <foxerlee1@gmail.com>
2020-09-27 23:38:31 -0400

    :bug: & :zap: fix permalink cross the border, support update without changing old _config.yml

---
+++ scripts/replace.js
@@ -0,0 +1,9 @@
+/**
+ * Note: configs in _data/temp.yml will replace configs in hexo.theme.config.
+ */
+hexo.on('generateBefore', function () {
+    if (hexo.locals.get) {
+      var data = hexo.locals.get('data') // 获取_data文件夹下的内容
+      data && data.temp && (hexo.theme.config = data.temp) // 如果temp.yml 存在，就把内容替换掉主题的config
+    }
+  })
